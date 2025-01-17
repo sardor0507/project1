87b29a9d0ff124159034fd0c4265d8614809d465
s.qudratullayev <s.qudratullayev@newuu.uz>
2025-01-17 10:24:32 +0500

    age asking

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int age;
+    cout << "Hello, I am Sardorbek and I am studying in Economics and Data Science major.\n";
+    cout << "Your Age:";
+    cin >> age;
+    cout <<"So, Your Age:"<<age<<endl;
+    return 0;
+}
