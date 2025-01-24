1a6e77f9a35d79033e35709bd14a80a8cd702bf9
s.qudratullayev <s.qudratullayev@newuu.uz>
2025-01-24 10:43:10 +0500

    1 to 4 problems

--- main.cpp
+++ main.cpp
@@ -13,5 +13,22 @@ int main() {
     //problem 2
     for (int i = 1; i <= 5; ++i)
         cout << setw(5) << i << endl;
+    //problem 3
+    int a, b;
+    cout << "Enter two integers: ";
+    cin >> a >> b;
+    // Swapping the values
+    int temp = a;
+    a = b;
+    b = temp;
+    cout << "Swapped values: " << "a = " << a << ", b = " << b << endl;
+    //problem 4
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+    cout << setw(10) << "Number" << setw(10) << "Square" << endl;
+    for (int i = 1; i <= n; ++i) {
+        cout << setw(10) << i << setw(10) << i * i << endl;
+    }
     return 0;
 }
